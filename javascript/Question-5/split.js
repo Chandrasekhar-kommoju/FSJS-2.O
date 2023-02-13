576802005464ea614fe1aadaac61ecac7ce19ae1
Chandra Sekhar <sekharc.cs27@gmail.com>
2023-02-13 15:39:37 +0530

    minor changes

--- javascript/Question-5/split.js
+++ javascript/Question-5/split.js
@@ -1,6 +1,6 @@
 // 05.. Declare a varibale and assign string value to it and then split it into an array using split() method
 
-let iNeuron = 'learning javascript from iNeuron'
+let iNeuron = 'learning javascript from iNeuron';
 
 let arr = iNeuron.split();
 
